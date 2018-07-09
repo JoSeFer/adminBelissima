ad8d1faeb7aa310fe7ac56191e303a5717a8aa92
Jose Antonio Parra <antoniojose558@gmail.com>
2018-07-09 17:40:42 -0400

    añadimos navbar rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,38 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { EmpresaListComponent } from './components/empresa-list/empresa-list.component';
+import { LoginPageComponent} from './components/login-page/login-page.component';
+import { RegisterPageComponent } from './components/register-page/register-page.component';
+import { AdminPageComponent } from './components/admin-page/admin-page.component';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+
 
 
 const routes: Routes = [
   {
     path: '',
-    component: HomeComponent
+    component: HomePageComponent
   },
   {
     path: 'listaEmpresa',
     component: EmpresaListComponent
+  },
+  {
+    path: 'login',
+    component: LoginPageComponent
+  },
+  {
+    path: 'register',
+    component: RegisterPageComponent
+  },
+  {
+    path: 'administrador',
+    component: AdminPageComponent
+  },
+  {
+    path: '**',
+    component: NotFoundPageComponent
   }
 ];
 
